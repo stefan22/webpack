07b0946575b2f7fd27dba10f0c4692c66efd6d9a
stefano1020 <stefano.pasquale@gmx.co.uk>
2018-12-01 17:29:24 +0000

    [cal fn] updated test function inside index.js

--- wp4.01/src/index.js
+++ wp4.01/src/index.js
@@ -1,35 +1,52 @@
-console.log('..from index66116.');
+console.log('..from index');
+console.clear();
 
 const operation = (op, x,y) => {
- const checkIsNum = (x,y) => {
-  if (typeof x == 'string' && typeof y == 'string') {
-    return x,y;
+ const checkIsValid = (op,x,y) => {
+  let isOpValid = (typeof op !== 'string') ? 'not valid' : op;
+  if (typeof x == 'number' && typeof y == 'number') {
+    if(isOpValid !== 'not valid') {
+     return x,y;
+    } else {
+      console.log('this op is not valid, please enter a valid op');
+      return true;
+    }
+  } else {
+    console.log('for an operation to be performed, pass numbers only.');
+    return true;
   }
  };
 
- switch (typeof op == 'string') {
-  case 'sum':
-   checkIsNum(x,y);
-   op = x + y;
-   console.log(op);
-   break;
-  case 'subs':
-   checkIsNum(x,y);
-   op = x - y;
-   console.log(op);
-   break;
-  case 'mult':
-   checkIsNum(x,y);
-   op = x * y;
-   console.log(op);
-   break;
-  default:
-    return 'you must enter one of these: "sum", "subs" or "mult" ';
- };
+ (() => {
+   switch (op) {
+     case 'sum':
+       checkIsValid(op, x, y);
+       op = x + y;
+       console.log(op);
+       break;
+     case 'subs':
+       checkIsValid(op, x, y);
+       op = x - y;
+       console.log(op);
+       break;
+     case 'mult':
+       checkIsValid(op, x, y);
+       op = x * y;
+       console.log(op);
+       break;
+     default:
+       console.log('at the moment we only do sum, subs, and mult. Try again');
+       break;
+   }
+   return true;
+})();
 
 };
+
+
 
 
 operation('sum', 6,5);
 operation('mult',5,4);
 operation('subs',9,8);
+operation('calc',3,4);
